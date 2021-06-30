35215567ca29ee6c5113e5e4dae62b309d722ba8
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-30 17:01:57 -0300

    Create lesson109.example.0.js

---
+++ lesson109.example.0.js
@@ -0,0 +1,14 @@
+/*
+
+Represents a precise date and/or 
+time in the proleptic Gregorian 
+calendar. The time element encodes modern
+dates and times in a machine-readable way, 
+so that, for example, user agents could 
+offer to add an event to the user's calendar.
+
+*/
+
+<time datetime="2007-08-29T13:58Z">
+  August 29th, 2007 at 13:58
+</time>
