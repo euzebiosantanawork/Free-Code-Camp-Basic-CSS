39c284e9b2e93f879f905bfc165cd1e27876dc92
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-11 17:22:38 -0300

    Create lesson123.example.js

---
+++ lesson123.example.js
@@ -0,0 +1,2 @@
+//The header and footer in the tweet embed example have child items that could be arranged as rows using the flex-direction property. This tells CSS to align the children horizontally.
+
